6c5a61bb147ea2a8bde8bfa187c92646efe58fa9
Angélica Cavalheiro Rodrigues <angelicacavalheiro51@gmail.com>
2021-11-28 11:12:24 -0300

    env-dev added in git ignore

--- tests/movimento.test.js
+++ tests/movimento.test.js
@@ -4,14 +4,9 @@ import connection from '../src/database/database.js';
 import { sessionsFactory } from '../src/factories/sessions.factory.js';
 import { movimentFactory } from '../src/factories/moviment.factory.js';
 
-//pra testar se o retorno de movimento esta correto, preciso antes de cada teste
-//inserir um usuario com token em sessions 
-//e um objeto valido em movimento
 beforeEach(async () => {
-
     await sessionsFactory();
     await movimentFactory();
-    
 });
 
 //depois de cada teste apago tudo de sessões
@@ -27,7 +22,7 @@ afterAll(() => {
 describe('GET /movimento', () => {
 
     it('returns 200 for valid user', async() => {
-    
+
         const result = await supertest(app)
             .get('/movimento')
             .set('Authorization', 'eb9f84ee-b54b-43f2-bca2-d3667d5820a7')
@@ -40,6 +35,6 @@ describe('GET /movimento', () => {
         expect(result.body[0]).toHaveProperty('saida')
         expect(result.body[0]).toHaveProperty('data')
         expect(result.body[0]).toHaveProperty('descricao')
-    })   
+    })
 })
 
